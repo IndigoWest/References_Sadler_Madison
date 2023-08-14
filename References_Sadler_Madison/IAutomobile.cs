d46a138b1f390889923209ef43dd6c608d306abe
MadisonSadler <MadisonSadler@DESKTOP-2U908NJ>
2023-08-14 09:16:06 -0500

    Add project files.

---
+++ References_Sadler_Madison/IAutomobile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_Sadler_Madison
+{
+    internal interface IAutomobile
+    {
+        public double Speed { get; }
+        public int Wheels { get; }
+        public string LicensePlate { get; }
+        public void Stringify();
+    }
+}
---
+++ References_Sadler_Madison/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using References_Sadler_Madison;
+
+Sedan myFirstSedan = new Sedan(0);
+IAutomobile myAutomobile = myFirstSedan;
+
+Sedan myOtherSedan = new Sedan(0);
+
+myFirstSedan.IncreaseSpeed();
+Console.WriteLine(myFirstSedan.Speed);
+Console.WriteLine(myAutomobile.Speed);
+Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+
+myOtherSedan.IncreaseSpeed();
+Console.WriteLine(myFirstSedan.Speed);
+Console.WriteLine(myOtherSedan.Speed);
+Console.WriteLine(myFirstSedan.Equals(myOtherSedan));
+
+Truck myTruck = new Truck(50, 500, "MyTrUcK");
+
+myFirstSedan.Stringify();
+myAutomobile.Stringify();
+myOtherSedan.Stringify();
+myTruck.Stringify();
